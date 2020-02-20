341b9cc6e67525469c124568417d7b6c2597e063
Alejandro Mogollon Medina <935@holbertonschool.com>
2020-02-19 19:57:31 -0500

    Creates a Puppet manifest that fixes and Apache server returning 500 error.

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# This puppet manifesto fixes a Wordpress bug
+exec { 'fix_error':
+  command => 'sed -i "s|.phpp|.php|g" /var/www/html/wp-settings.php',
+  path    => ['/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
+}
